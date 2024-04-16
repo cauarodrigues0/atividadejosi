bb4b9c6897b23a4ba113f69e16a357e44e326bcd
kethilly <kethillycrystiny07@gmail.com>
2024-04-16 14:36:41 -0300

    código concertado

--- pedido.py
+++ pedido.py
@@ -1,33 +1,26 @@
 class Pedido:
-    def __init__(self,itens,total,status):
+    def __init__(self, itens, total, status):
         self.itens = itens
-        self.total = total
+        self.precos = total 
         self.status = status
         
-    def item(self,item):
+    def add_item(self, item, preco):
         self.itens.append(item)
+        self.precos.append(preco)
     
-    def total(self):
-        self.item = 0
-        for item in self.itens:
-            self.item += item
-        return self.item
+    def calculate_total(self): 
+        total = sum(self.precos)
+        return total
 
-    def status(self,status):
-        print(f'entregue')
+    def get_status(self):  
+        return self.status
     
 if __name__ == '__main__':
-    pedido1 = Pedido('Camiseta',100)
-    pedido2 = Pedido('Calça',200)
+    pedido1 = Pedido(["Calça", "Camisa"], [100, 50], "entregue")
+    pedido2 = Pedido(["Legging", "Top"], [100, 30], "a caminho")
 
-    print(f'Total: {pedido2.total}')
-    print(f'Status: {pedido2.status}')
-    pedido2.status('Entregue')
-    print(f'Status: {pedido2.status}')
-
-    print(f'Total: {pedido1.total}')
-    print(f'Status: {pedido1.status}')
-    pedido1.status('Entregue')
-    print(f'Status: {pedido1.status}')
-
-    
+    pedido1.add_item("Short", 40)  
+    print(pedido1.calculate_total()) 
+    print(pedido2.calculate_total())
+    print(pedido1.get_status())
+    print(pedido2.get_status())  
